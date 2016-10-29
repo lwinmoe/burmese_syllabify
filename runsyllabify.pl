8cfa19dd972df9117cd6cefede9a312a558ed2b9
Lwin Moe <lwinmoe@gmail.com>
2016-10-29 15:41:02 -0400

    Initial commit.

---
+++ runsyllabify.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use Encode;
+use syllabify_burmese;
+
+my $input_folder = "input";
+my $output_folder = "output";
+
+while (<"$input_folder/*">) {
+  my $currF = $_;
+  my ($fileName) = /\/(.*)/;
+  print qq($fileName\n);
+  local $/;
+  open IN, "$currF" or die "can't read";
+  my $foo = <IN>;
+  close IN;
+  $foo = Encode::decode_utf8($foo);     # from bytes to UTF8 encoding
+  $foo = syllabify($foo);             # convert to Unicode 5.1
+                                        # other possible conversion routines are:
+                                        # soas2uni5, uni4touni5, myazedi2uni5, metta2uni5
+                                        # ava2uni5
+  $foo = Encode::encode_utf8($foo);     # back to bytes
+  open OUT, ">$output_folder/$fileName" or die;
+  print OUT $foo;
+  close OUT;
+}
